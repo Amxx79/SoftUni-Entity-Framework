a26955ea9ae26f56c63be8712d2560cb8edc2972
Amxx79 <e.siwanov@gmail.com>
2024-06-26 02:33:56 +0300

    Entity-Framework Introduction

---
+++ 01. Entity-Framework Introduction/Entity-Framework-Introduction/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            List<Employee> employees = context.Employees
+                .ToList();
+
+            var sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName}" +
+                    $"{employee.JobTitle} {employee.Salary:F2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
